8a9743350aa74cab55290fa7bf2dd3a079f1505e
Thomas Luk <thomasluk624@gmail.com>
2024-10-14 11:55:33 +0800

    first init
    Signed-off-by: Thomas Luk <thomasluk624@gmail.com>

---
+++ serial_hid_controller.py
@@ -0,0 +1,110 @@
+import serial
+from dataclasses import dataclass, field
+from typing import List
+from enum import Enum
+import time
+
+
+PORT = "/dev/ttyUSB0"
+BAUDRATE = 38400
+
+
+class HIDProperty(Enum):
+    HID_KEY = 0x01
+    HID_MKEY = 0x02
+    HID_MOUSE = 0x05
+
+
+@dataclass
+class Frame:
+    HEADER: int = field(default=0x2C, init=False)
+    data_length: int = field(init=False)
+    property: str
+    crc: int = field(init=False)
+    hid_data: List[int]
+
+    def __post_init__(self):
+        self.property = self.define_property(self.property)
+        self.data_length = self.calculate_data_length()
+        self.crc = self.calculate_crc()
+
+    def calculate_crc(self) -> int:
+        crc_sum = sum(self.hid_data) + self.property
+        return crc_sum & 0xFF
+
+    def calculate_data_length(self) -> int:
+        return len(self.hid_data)
+
+    def define_property(self, property_name: str) -> int:
+        try:
+            return HIDProperty[property_name].value
+        except KeyError:
+            raise ValueError(f"Invalid property name: {property_name}")
+
+    def to_bytes(self) -> bytes:
+        frame = [self.HEADER, self.data_length, self.crc, self.property] + self.hid_data
+        return bytes(frame)
+
+
+class SerialHIDController:
+    def __init__(self, port: str, baudrate: int):
+        self.port = port
+        self.baudrate = baudrate
+        self.ser = None
+        self.connect()
+
+    def connect(self):
+        try:
+            self.ser = serial.Serial(port=self.port, baudrate=self.baudrate)
+        except serial.SerialException as e:
+            print(f"Failed to open serial port {self.port}: {e}")
+            self.ser = None
+
+    def disconnect(self):
+        if self.ser and self.ser.is_open:
+            self.ser.close()
+
+    def send_frame(self, frame: Frame) -> bool:
+        if not self.ser or not self.ser.is_open:
+            print("Serial port is not oepn. Cannot send data frame.")
+            return False
+        frame_byte = frame.to_bytes()
+        try:
+            bytes_written = self.ser.write(frame_byte)
+            if bytes_written != len(frame_byte):
+                print("Failed to write complete frame to serial port.")
+                return False
+            return True
+        except serial.SerialException as e:
+            print(f"Failed to write to serial port: {e}")
+            return False
+
+    def volume_up(self):
+        frame = Frame(
+            property="HID_MKEY", hid_data=[0xE9, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        )
+        self.send_frame(frame)
+        frame = Frame(
+            property="HID_MKEY", hid_data=[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        )
+        self.send_frame(frame)
+
+    def volume_down(self):
+        frame = Frame(
+            property="HID_MKEY", hid_data=[0xEA, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        )
+        self.send_frame(frame)
+        frame = Frame(
+            property="HID_MKEY", hid_data=[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        )
+        self.send_frame(frame)
+
+
+serController = SerialHIDController(PORT, BAUDRATE)
+for i in range(10):
+    serController.volume_up()
+    time.sleep(1 / 30)
+for i in range(10):
+    serController.volume_down()
+    time.sleep(1 / 30)
+serController.disconnect()
